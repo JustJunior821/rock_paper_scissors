95e6e6a07f7c8ae8801fe230096bfd1220ebc75d
JustJunior82 <juniornguyen82@gmail.com>
2023-11-27 18:27:02 -0800

    Added JS to let user play game w/ buttons
    not finished: game doesn't track score beyond first press (how to loop?)

--- game.js
+++ game.js
@@ -17,7 +17,7 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection === "paper")
             return "You lose! Paper beats rock.";
         if (computerSelection === "scissors")
-            return "You win! Rock beats paper.";
+            return "You win! Rock beats scissors.";
     }
     else if (p_choice === "paper")
     {
@@ -40,25 +40,62 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
+
+
+// UI for Rock Paper Scissors
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const results_section = document.querySelector("#results");
+
+
+
+
 function game() 
 {
+    //Initial definitions
     let player_score = 0;
     let comp_score = 0;
-    for (let i = 0; i < 5; i++)
-    {
-        let player_choice = prompt("You're playing rock, paper, scissors! Please choose one of the three options.", "")
-        let computer_choice = getComputerChoice();
-        let result = playRound(player_choice, computer_choice);
-        console.log(result);
-        if (result[4] === "l")
-            comp_score++;
-        else if (result[4] === "w")
-            player_score++;
-    }
-    if (player_score > comp_score)
+    let result = "";
+    const score = document.createElement("h1");
+    score.textContent = "Player: 0 vs. Computer: 0";
+    results_section.appendChild(score);
+
+    
+    // Button Definitions
+    rock.addEventListener("click", () => {
+        result = playRound("rock", getComputerChoice());
+        const display = document.createElement("p");
+        display.textContent = result;
+        results_section.appendChild(display);
+    
+    });
+    
+    paper.addEventListener("click", () => {
+        result = playRound("paper", getComputerChoice());
+        const display = document.createElement("p");
+        display.textContent = result;
+        results_section.appendChild(display);
+    });
+    
+    scissors.addEventListener("click", () => {
+        result = playRound("scissors", getComputerChoice());
+        const display = document.createElement("p");
+        display.textContent = result;
+        results_section.appendChild(display);
+    });
+
+    // Scoring and Score Total:
+
+    if (result[4] === "l")
+        comp_score++;
+    else if (result[4] === "w")
+        player_score++;
+    score.textContent = `Player: ${player_score} vs. Computer: ${comp_score}`;
+
+    // Final Results
+    if (player_score === 5)
         return("You win! The final score was " + player_score + " to " + comp_score + ".");
-    else if (comp_score > player_score)
+    else if (comp_score === 5)
         return("You lose! The final score was " + player_score + " to " + comp_score + ".");
-    else
-        return("You tied! The final score was " + player_score + " to " + comp_score + ".");
 }
